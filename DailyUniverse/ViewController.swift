48bef735181ff6729767eade45ab2ee8264fe223
Pedro Muniz <phgmuniz@gmail.com>
2022-10-11 15:47:39 -0300

    First Commit. Deleted Storyboards

---
+++ DailyUniverse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyUniverse
+//
+//  Created by Pedro Muniz on 10/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DailyUniverseUITests/DailyUniverseUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DailyUniverseUITestsLaunchTests.swift
+//  DailyUniverseUITests
+//
+//  Created by Pedro Muniz on 10/10/22.
+//
+
+import XCTest
+
+final class DailyUniverseUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
